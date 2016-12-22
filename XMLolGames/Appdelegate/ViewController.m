e77a1662802a87bec977ef8dbe1169ae13115ecb
LoveCcheng <495737676@qq.com>
2016-12-22 15:22:17 +0800

    初始化

---
+++ XMLolGames/Appdelegate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XMLolGames
+//
+//  Created by montnets on 2016/12/22.
+//  Copyright © 2016年 montnets. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
